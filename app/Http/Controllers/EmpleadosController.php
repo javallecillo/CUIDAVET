b2bb5e5c50e9789e85f3923eeb3a913aef03c1eb
javallecillo <javallecillo@uth.hn>
2025-02-20 04:15:30 -0600

    primer usuario agregado

---
+++ app/Http/Controllers/EmpleadosController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Empleado;
+use Illuminate\Support\Facades\Hash;
+
+class EmpleadosController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        Empleado::create([
+            'nombre' => 'Jorge',
+            'apellido' => 'Vallecillo',
+            'telefono' => '96595294',
+            'tel_alternativo' => '',
+            'direccion' => 'Barrio El Centro',
+            'correo' => 'jorge@gmail.com',
+            'contrasenia' => Hash::make('1234'),
+            'id_rol' => 1,
+            'f_nacimiento' => '1999-12-12',
+            'genero' => 'M',
+            'foto' => '',
+            'f_contratacion' => '2021-12-12',
+            'id_departamento' => 1,
+            'dias_laborales' => 'Lunes,Martes,Miércoles',
+            'turno' => 'Tarde',
+            'salario' => 1000,
+            'id_moneda' => 1,
+            'estado' => 1,
+
+        ]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
---
+++ app/Models/Empleado.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Models;
+
+// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+
+class Empleado extends Authenticatable
+{
+    /** @use HasFactory<\Database\Factories\UserFactory> */
+    use HasFactory, Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var list<string>
+     */
+    protected $fillable = [
+        'nombre',
+        'apellido',
+        'telefono',
+        'tel_alternativo',
+        'direccion',
+        'correo',
+        'contrasenia',
+        'id_rol',
+        'f_nacimiento',
+        'genero',
+        'foto',
+        'f_contratacion',
+        'id_departamento',
+        'dias_laborales',
+        'turno',
+        'salario',
+        'id_moneda',
+        'estado',
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var list<string>
+     */
+    protected $hidden = [
+        'contrasenia',
+    ];
+
+    /*
+    // Relación con la tabla roles
+    public function rol()
+    {
+        return $this->belongsTo(Rol::class, 'id_rol');
+    }
+
+    // Relación con la tabla departamentos
+    public function departamento()
+    {
+        return $this->belongsTo(Departamento::class, 'id_departamento');
+    }
+
+    // Relación con la tabla monedas
+    public function moneda()
+    {
+        return $this->belongsTo(Moneda::class, 'id_moneda');
+    }*/
+}
